7b9b88e2fee8b5acefea549d5824284dc79097b9
BJ <bj@zo0d-2.local>
2019-05-31 18:59:18 -0400

    Init

---
+++ strings/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  strings
+//
+//  Created by BJ on 2019-05-31.
+//  Copyright © 2019 BJ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
